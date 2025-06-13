de11bc501bd8c0bdca6a9eb82c5be81cca307d63
smile <smiling.cornflower@gmail.com>
2025-06-14 03:22:56 +0500

    feat (user_favorite): Add GET favorites for other users

--- starthub/presentation/urls/user.py
+++ starthub/presentation/urls/user.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from presentation.views.user import MeFavoriteProjectsView, MeView, UserView
+from presentation.views.user import MeFavoriteProjectsView, MeView, UserView, UserFavoriteProjectsView
 
 urlpatterns = [
     path("me/", MeView.as_view(), name="user_me"),
+
     path("<int:user_id>/", UserView.as_view(), name="user_detail"),
-    path("me/favorites/", MeFavoriteProjectsView.as_view(), name="user_favorites"),
-    path("me/favorites/<int:project_id>/", MeFavoriteProjectsView.as_view(), name="user_favorites_with_project_id"),
+
+    path("<int:user_id>/favorites/", UserFavoriteProjectsView.as_view(), name="user_favorites"),
+
+    path("me/favorites/", MeFavoriteProjectsView.as_view(), name="me_favorites"),
+
+    path("me/favorites/<int:project_id>/", MeFavoriteProjectsView.as_view(), name="me_favorites_with_project_id"),
 ]
