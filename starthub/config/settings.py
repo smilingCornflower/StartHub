ebff36736a25c1684e0c4c37904b14bed2858b6b
smile <smiling.cornflower@gmail.com>
2025-06-19 16:49:52 +0500

    fix: DEFAULT_AUTHENTICATION_CLASSES equals to empty list

--- starthub/config/settings.py
+++ starthub/config/settings.py
@@ -162,6 +162,11 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+}
+
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
