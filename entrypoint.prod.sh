65f5c3fb8445cd22584eaee907d811320fbbb84a
Smile <smiling.cornflower@gmail.com>
2025-07-21 17:17:02 +0500

    chore: change workers amount to 5 in entrypoint.prod.sh

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -4,4 +4,5 @@
 cd starthub
 uv run manage.py collectstatic --noinput
 uv run manage.py migrate --noinput
-uv run gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi
+uv run gunicorn --bind 0.0.0.0:8000 --workers 5 config.wsgi
+
