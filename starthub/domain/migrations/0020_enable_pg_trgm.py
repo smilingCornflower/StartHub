25ec6513b806a399c3c034e1f46c3afc8f4403e9
Smile <smiling.cornflower@gmail.com>
2025-07-29 18:25:10 +0500

    chore(migrations): enable pg_trgm extension for trigram similarity

---
+++ starthub/domain/migrations/0020_enable_pg_trgm.py
@@ -0,0 +1,11 @@
+from django.contrib.postgres.operations import TrigramExtension
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("domain", "0019_remove_project_category_project_categories")
+    ]
+    operations = [
+        TrigramExtension()
+    ]
