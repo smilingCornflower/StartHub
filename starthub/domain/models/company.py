a2cfa1fcd43aa332572c48206f453d7b641fb4bc
Smile <smiling.cornflower@gmail.com>
2025-07-30 23:05:37 +0500

    feat (geo): add address field for Company

--- starthub/domain/models/company.py
+++ starthub/domain/models/company.py
@@ -1,5 +1,6 @@
 from autoslug import AutoSlugField
 from django.db import models
+
 from domain.constants import CHAR_FIELD_MAX_LENGTH, CHAR_FIELD_SHORT_LENGTH
 from domain.models.base import BaseModel
 
@@ -28,6 +29,7 @@ class Company(BaseModel):
     country = models.ForeignKey("domain.Country", on_delete=models.PROTECT)
     business_id = models.CharField(max_length=32, unique=True)
     established_date = models.DateField()
+    address = models.ForeignKey('domain.Address', null=True, on_delete=models.SET_NULL)
 
     class Meta:
         db_table = "company"
