e15bbfe402c47535bd8de852ebfd3a41e967fb07
Smile <smiling.cornflower@gmail.com>
2025-08-07 20:22:47 +0500

    feat (Incubator): add ProjectIncubator field

---
+++ starthub/domain/models/project_management/incubator.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from domain.constants import CHAR_FIELD_MAX_LENGTH
+from domain.models.base import BaseModel
+
+
+class ProjectIncubator(BaseModel):
+    project = models.OneToOneField('domain.Project', on_delete=models.CASCADE)
+    name = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH)
+    description = models.TextField()
+
+    class Meta:
+        db_table = "project_incubator"
+
+    def __str__(self) -> str:
+        return self.name
+
+    @classmethod
+    def get_permission_key(cls) -> str:
+        return "project_incubator"
