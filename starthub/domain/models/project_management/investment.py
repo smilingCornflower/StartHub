edd7069353cef4df8a10b332f39d592152d71389
Smile <smiling.cornflower@gmail.com>
2025-08-11 15:23:59 +0500

    feat(ProjectInvestment): add slug field for ProjectInvestment model

--- starthub/domain/models/project_management/investment.py
+++ starthub/domain/models/project_management/investment.py
@@ -1,11 +1,12 @@
 from django.db import models
 from domain.constants import CHAR_FIELD_MAX_LENGTH, CHAR_FIELD_MEDIUM_LENGTH
 from domain.models.base import BaseModel
-
+from autoslug import AutoSlugField
 
 class ProjectInvestment(BaseModel):
     project = models.ForeignKey("domain.Project", on_delete=models.CASCADE, related_name="investments")
     organization_name = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH)
+    slug = AutoSlugField(populate_from="organization_name", unique=True, max_length=CHAR_FIELD_MAX_LENGTH)
     amount = models.FloatField()
 
     class Meta:
