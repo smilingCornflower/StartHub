67a0f02498c23be9f4771fd36c1560928f5a47bc
Smile <smiling.cornflower@gmail.com>
2025-08-11 12:25:48 +0500

    feat(ProjectInvestment): add ProjectInvestment and ProjectInvestmentSocialLink models

--- starthub/domain/models/__init__.py
+++ starthub/domain/models/__init__.py
@@ -15,6 +15,8 @@ ProjectStep = import_module("domain.models.project_management.step").ProjectStep
 ProjectIncubator = import_module("domain.models.project_management.incubator").ProjectIncubator
 ProjectAccelerator = import_module("domain.models.project_management.accelerator").ProjectAccelerator
 ProjectCrowdfunding = import_module("domain.models.project_management.crowdfunding").ProjectCrowdfunding
+ProjectInvestment = import_module("domain.models.project_management.investment").ProjectInvestment
+ProjectInvestmentSocialLink = import_module("domain.models.project_management.investment").ProjectInvestmentSocialLink
 
 FundingModel = import_module("domain.models.project_management.funding_model").FundingModel
 Company = import_module("domain.models.company").Company
