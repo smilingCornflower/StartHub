8746c4677cad7744df141114741ab14a15ccb238
Smile <smiling.cornflower@gmail.com>
2025-08-12 19:51:23 +0500

    feat(GovernmentGrant): Grant links to Project via FK

--- starthub/domain/models/project_management/government_grant.py
+++ starthub/domain/models/project_management/government_grant.py
@@ -5,12 +5,15 @@ from domain.models.base import BaseModel
 
 
 class ProjectGovernmentGrant(BaseModel):
+    project = models.ForeignKey("domain.Project", on_delete=models.CASCADE, related_name="government_grants")
     grant_name = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH)
     grant_name_slug = AutoSlugField(populate_from="grant_name", unique=True, max_length=CHAR_FIELD_MAX_LENGTH)
 
     amount = models.FloatField()
     organization_name = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH)
-    organization_name_slug = AutoSlugField(populate_from="organization_name", unique=True, max_length=CHAR_FIELD_MAX_LENGTH)
+    organization_name_slug = AutoSlugField(
+        populate_from="organization_name", unique=True, max_length=CHAR_FIELD_MAX_LENGTH
+    )
 
     class Meta:
         db_table = "project_government_grants"
