5d3858f2590c12f19c4fd52d41ef2f3832ee09ff
Smile <smiling.cornflower@gmail.com>
2025-08-25 07:04:26 +0500

    fix(project_resubmit): add error response for resubmit exception

--- starthub/presentation/response_factories/report.py
+++ starthub/presentation/response_factories/report.py
@@ -1,5 +1,8 @@
+from domain.exceptions.project_management import ProjectResubmitException
 from presentation.response_factories.common import CommonErrorResponseFactory
 
 
 class ProjectReportErrorResponseFactory(CommonErrorResponseFactory):
-    error_codes = CommonErrorResponseFactory.error_codes | {}
+    error_codes = CommonErrorResponseFactory.error_codes | {
+        ProjectResubmitException: ("PROJECT_RESUBMIT_ERROR", 409),
+    }
