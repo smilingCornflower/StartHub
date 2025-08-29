3aa2b9ac5a3b3bbe457e6c944cd5adf06982e0a8
Smile <smiling.cornflower@gmail.com>
2025-08-29 13:39:47 +0500

    reformat(value_objects): validate country code via pycountry lib

--- starthub/domain/value_objects/country.py
+++ starthub/domain/value_objects/country.py
@@ -1,3 +1,4 @@
+import pycountry
 from domain.exceptions.geo.country import InvalidCountryCodeException
 from domain.value_objects import BaseVo
 from domain.value_objects.common import Id
@@ -15,6 +16,8 @@ class CountryCode(BaseVo):
     @classmethod
     def is_correct_country_code(cls, value: str) -> str:
         """:raises InvalidCountryCodeException:"""
-        if not (len(value) == 2 and value.isalpha() and value.isupper()):
-            raise InvalidCountryCodeException("Invalid country code")
+        if not value.isupper():
+            raise InvalidCountryCodeException("Country code must be uppercase ISO alpha-2")
+        if pycountry.countries.get(alpha_2=value) is None:
+            raise InvalidCountryCodeException(f"Invalid country code: {value}")
         return value
